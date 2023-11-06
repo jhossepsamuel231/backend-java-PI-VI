1015f437c5e76b604ffe4959177d9337d0048905
jhossepsamuel231 <jhossepsamuel23@gmail.com>
2023-11-06 03:53:32 -0500

    docker file

--- src/main/java/pe/edu/upeu/apipersonasuniversitarias/ApiPersonasUniversitariasApplication.java
+++ src/main/java/pe/edu/upeu/apipersonasuniversitarias/ApiPersonasUniversitariasApplication.java
@@ -2,9 +2,16 @@ package pe.edu.upeu.apipersonasuniversitarias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiPersonasUniversitariasApplication {
+
+		public String PORT = System.getenv("PORT");
+	public String MYSQL_URL = System.getenv("MYSQL_URL");
+	public String MYSQL_USERNAME = System.getenv("MYSQL_USERNAME");
+	public String MYSQL_PASSWORD = System.getenv("MYSQL_PASSWORD");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPersonasUniversitariasApplication.class, args);
